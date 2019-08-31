86ffc182efb9423781277ba01b953df42c15b66d
Haakon Ringdal <hakon.ringdal@gmail.com>
2019-08-31 23:41:50 +0200

    Initial commit

---
+++ lib/kallisto_view_tool.rb
@@ -0,0 +1,6 @@
+require "kallisto_view_tool/version"
+
+module KallistoViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kallisto_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KallistoViewTool
+  VERSION = "0.1.0"
+end
